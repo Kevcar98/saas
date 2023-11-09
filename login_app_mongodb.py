ebc3c49d6fc5d30d3a6dc2ff7b5a38be4ddc6f84
Kevcar98 <kevcarrod@gmail.com>
2023-11-09 20:10:37 +0000

    Added Data Entry Functionality

--- login_app_mongodb.py
+++ login_app_mongodb.py
@@ -13,6 +13,7 @@ from passlib.hash import pbkdf2_sha256
 client = MongoClient("mongodb+srv://Kevcar98:QKlGirVW2q6lYuuc@cluster0.bg2grjx.mongodb.net/")
 db = client["your_db_name"]  # Replace with your database name
 users_collection = db["users"]  # Replace with your collection name
+DataEntrys=db["DataEntry"]
 
 def LogOut():
     col1, col2, col3 = st.columns([0.26, 0.3, 0.1])
@@ -132,9 +133,92 @@ def home(username):
         menu_title= None,
         options=["GBP","Euro","USD"],
         icons=["1","2","3"],
-        orientation= "horizontal",)
+        orientation= "horizontal",)    
+    if selectedCurrency:
+        DataEntry(selectedCurrency,susername)
+    
+    
 
 
+
+def DataEntry(currency,username):
+
+    st.header(f"Data Entry in {currency}")
+    years = [datetime.today().year, datetime.today().year + 1]
+    months = list(calendar.month_name[1:])
+    incomes = ["Salary","Blog","Other Income"]
+    expenses = ["Rent","Utilities","Groceries","Car","Saving"]
+    comments=""
+    periods=""
+
+    with st.form("Entry_form", clear_on_submit=True):
+            col1, col2 = st.columns(2)
+            col1.selectbox("Select Month:", months, key= "month")
+            col2.selectbox("Select Year:", years, key="year")          
+
+            with st.expander("Income"):
+                for income in incomes:
+                    st.number_input(f"{income}:",min_value=0, format="%i", step=10,key=income)
+            with st.expander("Expenses"):
+                for expense in expenses:
+                    st.number_input(f"{expense}:", min_value=0,format="%i",step=10,key=expense)
+            with st.expander("Comment"):
+                comment = st.text_area("", placeholder="Enter a comment here ...")
+
+            submitted = st.form_submit_button("Save Data")
+            if submitted:
+                period = str(st.session_state["year"]) + " " + str(st.session_state["month"])
+                incomes = {income: st.session_state[income] for income in incomes}
+                expenses = {expense: st.session_state[expense] for expense in expenses}
+                data_entry = {"user":username,"period": period, "incomes": incomes,"expenses":expenses,"comments":comment}
+                DataEntrys.insert_one(data_entry)
+                st.success("Data Saved!")
+
+def get_all_periods():
+    items = db.fetch_all_periods()
+    periods = [item["key"] for item in items]
+    return periods
+
+def DataViz():
+    st.header("Data Visualization")
+    years = [datetime.today().year, datetime.today().year + 1]
+    months = list(calendar.month_name[1:])
+    incomes = ["Salary","Blog","Other Income"]
+    expenses = ["Rent","Utilities","Groceries","Car","Saving"]
+    with st.form("Saved periods"):
+            period = st.selectbox("Select Period:", get_all_periods())
+            submitted = st.form_submit_button("Plot Period")
+            if submitted:
+                period_data = DataEntrys.get_period(period)
+                for doc in period_data:
+                    comment = doc["comment"]
+                    expenses = doc["expenses"]
+                    incomes = doc["incomes"]
+
+
+                total_income = sum(incomes.values())
+                total_expense = sum(expenses.values())
+                remaining_budget = total_income - total_expense
+                col1, col2, col3 = st.columns(3)
+                col1.metric("Total Income",f"{total_income} {currency}")
+                col2.metric("Total Expense",f"{total_expense} {currency}")
+                col3.metric("Remaining Budget",f"{remaining_budget} {currency}")
+                st.text(f"Comment:{comment}")
+
+
+                label = list(incomes.keys()) + ["Total income"] + list(expenses.keys())
+                source = list(range(len(incomes))) + [len(incomes)] * len(expenses)
+                target = [len(incomes)] * len(incomes) + [label.index(expense) for expense in expenses.keys()]
+                value = list(incomes.values()) + list(expenses.values())
+
+
+                link = dict(source=source, target=target,value=value)
+                node = dict(label=label,pad=20,thickness=25,color="#00684A")
+                data = go.Sankey(link=link,node=node)
+
+                fig = go.Figure(data)
+                fig.update_layout(margin=dict(l=0,r=0,t=5,b=5))
+                st.plotly_chart(fig, use_container_width=True)
 
 
 
